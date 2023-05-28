df3ebb44c014592dcae38ef4f86f92608dc2fbb2
ifczt <ifczt@qq.com>
2023-05-28 10:52:48 +0800

    init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,8 @@
 # Author: IFCZT
 # Email: ifczt@qq.com
+import hashlib
 import re
+from random import random
 
 
 def path_to_key(path):
@@ -16,3 +18,8 @@ def icompose(*funs):
         return f
 
     return deco
+
+
+# 生成密码
+def encryption_md5(value: [str, bool]):
+    return hashlib.md5(value.encode(encoding='UTF-8')).hexdigest()
